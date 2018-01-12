0ae751fb728ca0a9f071f67c10b566d1737f7a0e
Anagha Parkhi <anaghapathak2008@gmail.com>
2018-01-12 05:37:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,19 @@ function shout(string){
   return string.toUpperCase()
 }
 
-sayHiToGrandma()
+sayHiToGrandma(){
+  
+  var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
+}
